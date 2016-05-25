513d00e4065e3cda7a26a204665d3778c71524c7
Cloquet <alban.cloquet@epitech.eu>
2016-05-25 22:28:03 +0200

    Aller les models 3ds

--- src/Game.cpp
+++ src/Game.cpp
@@ -17,9 +17,9 @@ Bomberman::Game::Game() : irr(Bomberman::Irrlicht::instance())
 
   _m_scene->setMaterialTexture(0, irr.getDriver()->getTexture("./media/Te/Wall_Normal.JPG"));
 
-  scene::IAnimatedMesh  * man = irr.getSmgr()->getMesh("./media/Bomberman/Bomberman.3ds");
+  scene::IAnimatedMesh  * man = irr.getSmgr()->getMesh("./media/Bomberman/BomberMan.3ds");
   irr.getSmgr()->getParameters()->setAttribute(scene::CSM_TEXTURE_PATH, "./media/Bomberman");
-  irr.getSmgr()->addAnimatedMeshSceneNode(man);
+  irr.getSmgr()->addAnimatedMeshSceneNode(man, _m_scene, -1, core::vector3df(0,0,0), core::vector3df(-120, 0, 0), core::vector3df(0.05f, 0.05f, 0.05f));
   /*scene::IMesh *cube = irr.getSmgr()->getGeometryCreator()->createCubeMesh(core::vector3df(5.f,5.f,5.f));
   _scene = irr.getSmgr()->addMeshSceneNode(plan, _scene);
   irr.getSmgr()->addMeshSceneNode(cube, _scene);*/
@@ -27,18 +27,9 @@ Bomberman::Game::Game() : irr(Bomberman::Irrlicht::instance())
 
 Bomberman::Game::~Game()
 {
-/*
- * // La salle
-scene::IAnimatedMesh *room = scenegraph->getMesh("room.3ds");
-scene::IMeshSceneNode *Nroom = scenegraph->addMeshSceneNode(room->getMesh(0));
-//On desactive la sensibilite a la lumiere
-Nroom->setMaterialFlag(video::EMF_LIGHTING, true);
-//On definit le nombre de repetition de la texture
-scenegraph->getMeshManipulator()->makePlanarTextureMapping(room->getMesh(0), 0.04f);
-//On choisit la texture
-Nroom->setMaterialTexture( 0, driver->getTexture("rockwall.bmp") );
- */
 }
+
+
 
 void 		Bomberman::Game::run()
 {
@@ -47,6 +38,7 @@ void 		Bomberman::Game::run()
   //irr.getSmgr()->addCameraSceneNodeFPS();
   while (irr.getDevice()->run())
     {
+
       irr.getDriver()->beginScene(true, true, video::SColor(255,100,101,140));
       irr.getSmgr()->drawAll();
       irr.getGui()->drawAll();
