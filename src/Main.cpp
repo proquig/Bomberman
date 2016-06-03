23770d1afb50827b661474a13a80acffb1631560
Gauthier POGAM--LE MONTAGNER <gauthier.pogam-lemontagner@epitech.eu>
2016-06-03 19:13:42 +0200

    Blaming regex.

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,6 +6,16 @@
 #include "Main.hpp"
 #include "Game.hpp"
 
+#ifdef _IRR_WINDOWS_
+#pragma comment(lib, "Irrlicht.lib")
+#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+#endif
+
+int main()
+{
+  new Bomberman::Main();
+}
+
 Bomberman::Main::Main() : _irrlicht(Irrlicht::instance())
 {
   Menu menu;
