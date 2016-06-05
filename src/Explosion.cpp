193b3deb6d631c7ed5834ae4a0ce2ec1a80bded5
Cloquet <alban.cloquet@epitech.eu>
2016-06-05 18:44:24 +0200

    AngryBirds

--- src/Explosion.cpp
+++ src/Explosion.cpp
@@ -9,36 +9,17 @@ Bomberman::Explosion::Explosion(float x, float y) : _irr(Bomberman::Irrlicht::in
 						    _x(x),
 						    _y(y)
 {
-  std::cout << "B2!" << std::endl;
-  this->getScene();
+
+  this->node = this->_irr.getSmgr()->addMeshSceneNode(
+	  this->_irr.getSmgr()->getMesh("./assets/Bomb/Blacksmith/Blacksmith.DAE"),
+						       0, -1, irr::core::vector3df(this->_x, 1, this->_y),
+						       irr::core::vector3df(0, 0 , 0),
+						       irr::core::vector3df(1, 1, 1));
 }
 
-
-void 			Bomberman::Explosion::getScene()
+void 			Bomberman::Explosion::clear()
 {
-  irr::video::ITexture *titre = _irr.getDriver()->getTexture("./assets/Bomb/Explosion.jpg");
-
-  _irr.getDriver()->draw2DImage(titre, irr::core::rect<irr::s32>(_x - 10, _y - 5,
-								 _x + 10, _y + 5),
-				irr::core::rect<irr::s32>(_x - 10, _y - 5, _x + 10, _y + 5), 0, 0, true);
-  //this->_ps = this->_irr.getSmgr()->addParticleSystemSceneNode(false);
-  //  this->_em = _ps->createBoxEmitter(irr::core::aabbox3d<irr::f32>(-7,0,-7,7,1,7),
-  //				    irr::core::vector3df(0.0f,0.0f,0.0f), 10, 50,
-  //				    irr::video::SColor(0,255,255,255),	\
-  //				    irr::video::SColor(0,255,255,255), 800, 2000, 0,
-  //				    irr::core::dimension2df(1.f,1.f), irr::core::dimension2df(2.f,2.f));
-  //  _ps->setEmitter(this->_em);
-  //  _em->drop();
-  //  _paf = _ps->createFadeOutParticleAffector();
-  //  _ps->addAffector(_paf);
-  //  _paf->drop();
-  //  _ps->setPosition(irr::core::vector3df(this->_x, 0, this->_y));
-  //  _ps->setScale(irr::core::vector3df(0.5, 0.5, 0.5));
-  //  _ps->setMaterialFlag(irr::video::EMF_LIGHTING, false);
-  //  _ps->setMaterialFlag(irr::video::EMF_ZWRITE_ENABLE, false);
-  //  _ps->setMaterialTexture(0, _irr.getDriver()->getTexture("./assets/Bomb/Explosion.jpg"));
-  //  _ps->setMaterialType(irr::video::EMT_TRANSPARENT_ADD_COLOR);
-  std::cout << "Boom !" << std::endl;
+  node->remove();
 }
 
 Bomberman::Explosion::~Explosion()
