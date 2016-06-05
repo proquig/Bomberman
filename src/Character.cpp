14704f9ea7e786424da57647559dd84adae7101f
Guillaume PROQUIN <guillaume.proquin@epitech.eu>
2016-06-05 22:21:06 +0200

    HandleEvents?

--- src/Character.cpp
+++ src/Character.cpp
@@ -13,7 +13,7 @@
 #include "IA.hpp"
 
 Bomberman::Character::Character(const std::string &mesh_path, const std::string &texture_path, float x, float y, Bomberman::TYPE type) :
-  Bomberman::Obj::Obj(mesh_path, texture_path, x, y, Bomberman::CHARACTER), _ia(nullptr)
+	Bomberman::Obj::Obj(mesh_path, texture_path, x, y, Bomberman::CHARACTER), _ia(nullptr)
 {
   this->_bombTime = 0;
   this->_godTime = 0;
@@ -28,22 +28,22 @@ Bomberman::Character::~Character()
   delete this->_ia;
 }
 
-void							Bomberman::Character::set_pos(ACTION direction)
+void                                                        Bomberman::Character::set_pos(ACTION direction)
 {
-      this->_x += (Bomberman::Game::positions[direction][0] * this->_speed);
-      this->_y += (Bomberman::Game::positions[direction][1] * this->_speed);
-      this->set_orientation(direction);
-      this->_animated_node->setPosition(irr::core::vector3df(this->_x, 0, this->_y));
-      if (this->_animated_node->getStartFrame() == START_FRAME)
-	{
-	  this->_animated_node->setFrameLoop(START_WALK_FRAME, END_WALK_FRAME);
-	  this->afk();
-	}
+  this->_x += (Bomberman::Game::positions[direction][0] * this->_speed);
+  this->_y += (Bomberman::Game::positions[direction][1] * this->_speed);
+  this->set_orientation(direction);
+  this->_animated_node->setPosition(irr::core::vector3df(this->_x, 0, this->_y));
+  if (this->_animated_node->getStartFrame() == START_FRAME)
+    {
+      this->_animated_node->setFrameLoop(START_WALK_FRAME, END_WALK_FRAME);
+      this->afk();
+    }
 }
 
-void							Bomberman::Character::set_orientation(ACTION direction)
+void                                                        Bomberman::Character::set_orientation(ACTION direction)
 {
-  int			orientation[4];
+  int orientation[4];
 
   orientation[GO_UP] = 0;
   orientation[GO_DOWN] = 180;
@@ -52,15 +52,15 @@ void							Bomberman::Character::set_orientation(ACTION direction)
   this->_animated_node->setRotation(irr::core::vector3df(0, orientation[direction], 0));
 }
 
-void							Bomberman::Character::add_bomb(Bomberman::Bomb* bomb)
+void                                                        Bomberman::Character::add_bomb(Bomberman::Bomb *bomb)
 {
   bomb->setRange(&this->_range);
   this->_bombs.push_back(bomb);
 }
 
-void							Bomberman::Character::put_bomb(ACTION action)
+void                                                        Bomberman::Character::put_bomb(ACTION action)
 {
-  int							i;
+  int i;
 
   i = -1;
   if (this->_irr.getDevice()->getTimer()->getTime() > this->_bombTime)
@@ -74,13 +74,13 @@ void							Bomberman::Character::put_bomb(ACTION action)
     }
 }
 
-void							Bomberman::Character::afk()
+void                                                        Bomberman::Character::afk()
 {
-  AnimationEndCallback	*callback = new AnimationEndCallback();
+  AnimationEndCallback *callback = new AnimationEndCallback();
   this->_animated_node->setAnimationEndCallback(callback);
 }
 
-void							Bomberman::Character::jump(ACTION action)
+void                                                        Bomberman::Character::jump(ACTION action)
 {
   if (this->_animated_node->getStartFrame() == START_FRAME)
     {
@@ -89,41 +89,41 @@ void							Bomberman::Character::jump(ACTION action)
     }
 }
 
-void							Bomberman::Character::do_action(ACTION action)
+void                                                        Bomberman::Character::do_action(ACTION action)
 {
-  CharMemFn					actions[] = {
-    &Character::set_pos,
-    &Character::set_pos,
-    &Character::set_pos,
-    &Character::set_pos,
-    &Character::put_bomb,
-    &Character::jump,
+  CharMemFn actions[] = {
+	  &Character::set_pos,
+	  &Character::set_pos,
+	  &Character::set_pos,
+	  &Character::set_pos,
+	  &Character::put_bomb,
+	  &Character::jump,
   };
   (this->*actions[action])(action);
 }
 
-void							Bomberman::Character::setBombRange()
+void                                                        Bomberman::Character::setBombRange()
 {
   this->_range++;
 }
 
-irr::u32						Bomberman::Character::getGodTime() const
+irr::u32                                                Bomberman::Character::getGodTime() const
 {
   return (this->_godTime);
 }
 
-void 							Bomberman::Character::setGodTime(irr::u32 time)
+void                                                        Bomberman::Character::setGodTime(irr::u32 time)
 {
   this->_is_destructible = (!time);
   this->_godTime = time;
 }
 
-irr::u32						Bomberman::Character::getSpeedTime() const
+irr::u32                                                Bomberman::Character::getSpeedTime() const
 {
   return (this->_speedTime);
 }
 
-void 							Bomberman::Character::setSpeedTime(irr::u32 time)
+void                                                        Bomberman::Character::setSpeedTime(irr::u32 time)
 {
   this->_speed = time ? 2 : 1;
   this->_speedTime = time;
@@ -133,7 +133,6 @@ int Bomberman::Character::getSpeed() const
 {
   return (this->_speed);
 }
-
 
 void Bomberman::Character::setIa(Bomberman::IA *ia)
 {
